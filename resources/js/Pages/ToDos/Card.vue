eabbcf2be3f7057b2bf03ea54e9c2efa9cd75489
Caique Bertolozzi <caique.bertolozzi@agv.com.br>
2021-02-17 00:06:44 -0300

    Create Todos List Page

---
+++ resources/js/Pages/ToDos/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="p-2 sm:px-4 bg-white border-b border-gray-200">
+            <div class="flex flex-row">
+                <div class="flex-1">
+                    <h2 class="text-lg font-bold">{{todo.description}}</h2>
+                    <p>5 / 10 ITEMS</p>
+                </div>
+                <div class="flex-none self-center">
+                    <button type="button" class="border-2 rounded-full p-1 hover:border-black focus:none">
+                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            todo: Object
+        }
+    }
+</script>
--- resources/js/Pages/ToDos/List.vue
+++ resources/js/Pages/ToDos/List.vue
@@ -6,32 +6,10 @@
             </h2>
         </template>
 
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        <div class="p-2 sm:px-4 bg-white border-b border-gray-200">
-                            <div class="flex flex-row">
-                                <div class="flex-1">
-                                    <h2 class="text-lg font-bold">To Do 1</h2>
-                                </div>
-                                <div class="flex-none p-2 h-12 w-12">
-                                    <button type="button" class="h-12 w-12">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                                        </svg>
-                                    </button>
-
-                                    <!-- <div class="border-2 border-gray-500 rounded-full items-center justify-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                                        </svg>
-                                    </div> -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <todo-card :key="todo.id" v-for="todo in todos" :todo="todo"></todo-card>
                 </div>
             </div>
         </div>
@@ -41,11 +19,40 @@
 <script>
     import AppLayout from '@/Layouts/AppLayout';
     import Button from '@/Jetstream/Button';
+    import TodoCard from './Card';
 
     export default {
         components: {
             AppLayout,
-            Button
+            Button,
+            TodoCard,
+        },
+
+        data: function() {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        description: 'To Do 1',
+                        slug: 'to-do-1'
+                    },
+                    {
+                        id: 2,
+                        description: 'To Do 2',
+                        slug: 'to-do-2'
+                    },
+                    {
+                        id: 3,
+                        description: 'To Do 3',
+                        slug: 'to-do-3'
+                    },
+                    {
+                        id: 4,
+                        description: 'To Do 4',
+                        slug: 'to-do-4'
+                    },
+                ]
+            }
         }
     }
 </script>
